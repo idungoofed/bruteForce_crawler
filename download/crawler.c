a04e5f26205ba1f039b3e8478b53fb2108246200
Mark Philip <philip.mark123@gmail.com>
2016-07-18 02:08:20 +0200

    Synchronized write part of threads, added way to quit

--- download/crawler.c
+++ download/crawler.c
@@ -2,24 +2,72 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <unistd.h>
 #include "crawler.h"
 
 /**
 Hardcoded for http://puu.sh/FILENAME, where FILENAME is 6 characters long.
 In essence, a more optimized version of my old puush crawler.
 **/
-void *check(void *fileName) {
-  fileName = (char *)fileName;
-  char* sysStr = (char *)malloc(sizeof(char) * ((int)strlen(command) + 11));
-  strncpy(sysStr, command, strlen(command) + 1);
-  strncat(sysStr, fileName, 6);
-  strncat(sysStr, ".jpg", 4);
-  printf("Command string: %s\n", sysStr);
-  counter--;
-  //free(fileName);
-  free(sysStr);
-  return NULL;
+
+void *writeControl(void *notNeeded) {
+  while(run) {
+    if (turn_to_write >= MAXTHREADS) {
+      turn_to_write = turn_to_write%MAXTHREADS;
+    }
+  }
+  printf("Program exiting\n");
+  pthread_exit(NULL);
 }
+
+void *check(void *indexParam) {
+  //initialize the string to be used with the system() call
+  int startIndex = (int)(*((int *)indexParam));
+  char* sysStr = (char *)malloc(sizeof(char) * ((int)cmdLen + 11));
+  strncpy(sysStr, command, cmdLen);
+  sysStr[cmdLen+6] = '.';
+  sysStr[cmdLen+7] = 'j';
+  sysStr[cmdLen+8] = 'p';
+  sysStr[cmdLen+9] = 'g';
+  sysStr[cmdLen+10] = '\0';
+  
+  //create the filename, store into correct position in sysStr
+  int a, b, c, d, e, f;
+  while (run) {
+    for (a = 0; a < 62; a+=MAXTHREADS) {
+      sysStr[cmdLen] = charSet[a];
+      for (b = 0; b < 62; b+=MAXTHREADS) {
+	sysStr[cmdLen+1] = charSet[b];
+	for (c = 0; c < 62; c+=MAXTHREADS) {
+	  sysStr[cmdLen+2] = charSet[c];
+	  for (d = 0; d < 62; d+=MAXTHREADS) {
+	    sysStr[cmdLen+3] = charSet[d];
+	    for (e = 0; e < 62; e+=MAXTHREADS) {
+	      sysStr[cmdLen+4] = charSet[e];
+	      for (f = startIndex; f < 62; f+=MAXTHREADS) {
+		sysStr[cmdLen+5] = charSet[f];
+		//system(sysStr)
+		//check
+		//wait for turn, then write
+		//remove
+		while (turn_to_write != startIndex) {}
+#ifdef DEBUG
+		printf("String %d, command = %s\n", startIndex, sysStr);
+#endif
+		turn_to_write++;
+	      }
+	    }
+	  }
+	}
+      }
+    }
+  }
+  free(sysStr);
+  free(indexParam);
+  if (startIndex == MAXTHREADS - 1) {exit(0);}
+  else {pthread_exit(NULL);}
+}
+
 
 int main(int argc, char* argv[]) {
 
@@ -27,17 +75,18 @@ int main(int argc, char* argv[]) {
   outFile = fopen("results.txt", "w");
   //leave space in command for the URL to be copied into by the thread
 #ifdef QUIET
-  command = (char *)malloc(sizeof(char) * 22);
-  strncpy(command, "wget -q http://puu.sh/", 22);
+  command = (char *)malloc(sizeof(char) * 23);
+  strcpy(command, "wget -q http://puu.sh/\0");
+  cmdLen = 22;
 #endif
 #ifdef VERBOSE
-  command = (char *)malloc(sizeof(char) * 19);
-  strncpy(command, "wget http://puu.sh/", 19);
+  command = (char *)malloc(sizeof(char) * 20);
+  strcpy(command, "wget http://puu.sh/\0");
+  cmdLen = 19;
 #endif
   
   
   //generate charSet
-  char charSet[62];
   int i;
   for (i = 48; i < 58; i++) {
     charSet[i-48] = i;
@@ -50,53 +99,36 @@ int main(int argc, char* argv[]) {
   }
 
   //initialize thread stuff
-  pthread_t *pthread = malloc(sizeof(pthread_t));
   pthread_attr_t attr;
   pthread_attr_init(&attr);
   pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
-  
+  //pthread_t *pthread = malloc(sizeof(pthread_t));
+  pthread_t pthread;
   
 #ifdef DEBUG
   printf("Command string: \"%s\"\n", command);
-  printf("Size = %d\n", (int)strlen(command));
-    
+  //printf("Size = %d\n", (int)strlen(command));
 #endif
 
-  char *fn = (char *)malloc(sizeof(char) * 6);
-  int a, b, c, d, e, f;
-  for (a = 0; a < 62; a++) {
-    fn[0] = charSet[a];
-    for (b = 0; b < 62; b++) {
-      fn[1] = charSet[b];
-      for (c = 0; c < 62; c++) {
-	fn[2] = charSet[c];
-	for (d = 0; d < 62; d++) {
-	  fn[3] = charSet[d];
-	  for (e = 0; e < 62; e++) {
-	    fn[4] = charSet[e];
-	    for (f = 0; f < 62; f++) {
-	      fn[5] = charSet[f];
-#ifdef DEBUG
-	      if (counter >= 8) printf("Waiting. Currently running: %d\n", counter);
-#endif
-	      while (counter >= 8) {} //blocking so at most 8 threads run at once
-#ifdef DEBUG
-	      printf("Resuming. Currently running: %d\n", counter+1);
-#endif
-	      counter++;
-	      //char *tmpFN = (char *)malloc(sizeof(char) * 6);
-	      //strncpy(tmpFN, fn, 6);
-	      printf("Thread starting with string: %s\n", fn);
-	      pthread_create(pthread, &attr, check, (void *)fn);
-	    }
-	  }
-	}
-      }
-    }
-  }		      
-  free(pthread);
+  //start thread that controls writing to outFile
+  turn_to_write = 0;
+  pthread_create(&pthread, &attr, writeControl, (void *)(NULL));
+
+  //start download threads
+  //precondition: len(charSet) > MAXTHREADS
+  for (i = 0; i < MAXTHREADS; i++) {
+    int *arg = (int *)malloc(sizeof(*arg));
+    *arg = i;
+    pthread_create(&pthread, &attr, check, (void *)arg);
+  }
+
+  //while stopFIle doesn't exist, wait
+  while(access(stopFile, F_OK) == -1) {}
+  run = 0;
+  pthread_attr_destroy(&attr);
+  //free(pthread);
   free(command);
   fclose(outFile);
-  free(fn);
   pthread_exit(NULL);
 }
+  
--- download/crawler.h
+++ download/crawler.h
@@ -1,6 +1,17 @@
 #define QUIET
 //#define VERBOSE
-static int outBlock = 0;
+
+//number of threads that will be spawned
+const static int MAXTHREADS = 8;
+//name of file to create in order to tell the program to stop
+const static char* stopFile = "stop";
+
+//flags for the spawned threads
+static int turn_to_write;
+static int run = 1;
+
+//ensures all threads can access data
 static FILE *outFile;
-static int counter = 0;
-char* command;
+static char* command;
+static int cmdLen;
+static char charSet[62];
